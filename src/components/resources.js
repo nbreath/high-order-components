a634be3d4c053aae700f97f4490302200a86d174
John <john@kommit.co>
2017-10-19 16:08:48 -0500

    Add tab and components
    Section 4 class 37. Advanced React-Redux

---
+++ src/components/resources.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default () => {
+  return (
+    <div>
+      Super special secret recipe!
+
+      <ul>
+        <li>1 cup suggar</li>
+        <li>1 cup peper</li>
+        <li>1 cup salt</li>
+      </ul>
+    </div>
+  );
+};
